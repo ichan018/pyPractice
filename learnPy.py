56a034b772f5b47b3700751771188fba486e6c1d
Ivan Chan <ichan018@ucr.edu>
2020-06-10 16:50:45 -0700

    Store user input

--- learnPy.py
+++ learnPy.py
@@ -1,6 +1,11 @@
+
+"""
+This file, given a name entered by the user and the current date, prints out a line of code with such information.
+"""
+
 import datetime
 
-print("Hello world")
+#print("Hello world")
 
 myName = input("Enter your name")
 
@@ -9,3 +14,16 @@ w = datetime.date.today();
 myLine = "On {}, {} did something fun."
 
 print(myLine.format(w, myName))
+
+numInTuple = int(input("How many values do you want in the tuple?"))
+
+print(numInTuple) #print for debugging
+
+currTuple = ();
+for x in range(numInTuple):
+    newTupleInput = input("Enter new input");
+
+    newTuple = (newTupleInput,)
+    currTuple = currTuple + newTuple
+
+print(currTuple)
