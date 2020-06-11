3438e1f23432edb189d867fea2b753f40286f7bf
Ivan Chan <ichan018@ucr.edu>
2020-06-10 17:16:24 -0700

    Create first function

--- learnPy.py
+++ learnPy.py
@@ -4,8 +4,31 @@ This file, given a name entered by the user and the current date, prints out a l
 """
 
 import datetime
+thePwd = (1,7,6)
 
 #print("Hello world")
+def check_pwd():
+   
+   currTuple = ();
+
+   print("Enter 3 integers")
+   for x in range(numInTuple):
+       newTupleInput = int(input(""));
+
+       newTuple = (newTupleInput,)
+       currTuple = currTuple + newTuple
+
+   print(currTuple);
+  
+   #compare tuples
+   isPwd = 1
+   for x in range(len(currTuple)):
+       if currTuple[x] != thePwd[x]:
+          isPwd = 0
+       print(currTuple[x])
+   return isPwd;
+    
+
 
 myName = input("Enter your name")
 
@@ -15,15 +38,14 @@ myLine = "On {}, {} did something fun."
 
 print(myLine.format(w, myName))
 
-numInTuple = int(input("How many values do you want in the tuple?"))
+#numInTuple = int(input("How many values do you want in the tuple?"))
+numInTuple = 3;
 
-print(numInTuple) #print for debugging
 
-currTuple = ();
-for x in range(numInTuple):
-    newTupleInput = input("Enter new input");
+if check_pwd() == 1:
+    print("Correct password")
+else:
+    print("Wrong password")
+  
 
-    newTuple = (newTupleInput,)
-    currTuple = currTuple + newTuple
 
-print(currTuple)
